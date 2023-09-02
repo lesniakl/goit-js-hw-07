4c0cf64d64a1dd3d7b0139796d2d47e7417c7faf
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-09-02 17:16:41 +0200

    task 1 complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+renderGallery();
 
-console.log(galleryItems);
+function renderGallery() {
+  const galleryContent = [];
+  for (let i = 0; i <= galleryItems.length - 1; i++) {
+    const newItem = document.createElement("div");
+    newItem.classList.add("gallery__item");
+
+    const newLink = document.createElement("a");
+    newLink.classList.add("gallery__link");
+    newLink.href = galleryItems[i].original;
+
+    const newImg = document.createElement("img");
+    newImg.classList.add("gallery__image");
+    newImg.src = galleryItems[i].preview;
+    newImg.dataset.source = galleryItems[i].original;
+    newImg.alt = galleryItems[i].description;
+
+    newLink.append(newImg);
+    newItem.append(newLink);
+    galleryContent.push(newItem);
+  }
+  gallery.append(...galleryContent);
+}
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) return;
+  const closeHandler = (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  };
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onShow: (instance) => gallery.addEventListener("keydown", closeHandler),
+      onClose: (instance) =>
+        gallery.removeEventListener("keydown", closeHandler),
+    }
+  );
+  instance.show();
+});
